7cd0e77e7b87464390d3710b03390d72a0af695e
simonwillcock <simon@willcock.com.au>
2013-08-08 12:01:28 +0300

    Decided to work with JSON to make addition of HTML display easier!

--- weather.py
+++ weather.py
@@ -1,6 +1,8 @@
 """ 
-Use the OpenWeather API to get the weather for a given city and print out the results
+Use the OpenWeather API (http://openweathermap.org/api) to get the weather for a given city and print out the results
 Reddit link: http://www.reddit.com/r/beginnerprojects/comments/1dzbu7/project_whats_the_weather/
 """
 
-def getWeatherForCity(city):
+import json
+
+
